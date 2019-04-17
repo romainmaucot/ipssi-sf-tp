11ba41c0d5c495868d790b196d23bc3f8d1a53f7
Romain Moko <maucotel.romain@gmail.com>
2019-04-17 15:51:50 +0200

    header

--- var/cache/dev/twig/5b/5b5641993be6b214d37a5d98ca5f1cf80018e066770b701be67095044daf2753.php
+++ var/cache/dev/twig/5b/5b5641993be6b214d37a5d98ca5f1cf80018e066770b701be67095044daf2753.php
@@ -56,14 +56,40 @@ class __TwigTemplate_aaf7a164deffea816be7ead945a795e51c557aa319328d00efda08a89a2
         $this->displayBlock('stylesheets', $context, $blocks);
         // line 10
         echo "    </head>
+    <header>
+        <nav class=\"navbar navbar-expand-lg navbar-dark bg-primary\">
+            <a class=\"navbar-brand\" href=\"#\">Navbar</a>
+            <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarColor01\" aria-controls=\"navbarColor01\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+                <span class=\"navbar-toggler-icon\"></span>
+            </button>
+
+            <div class=\"collapse navbar-collapse\" id=\"navbarColor01\">
+                <ul class=\"navbar-nav mr-auto\">
+                    <li class=\"nav-item active\">
+                        <a class=\"nav-link\" href=\"#\">Blog</a>
+                    </li>
+                    <li class=\"nav-item\">
+                        <a class=\"nav-link\" href=\"/login\">Connexion</a>
+                    </li>
+                    <li class=\"nav-item\">
+                        <a class=\"nav-link\" href=\"/account\">Mon compte</a>
+                    </li>
+                    <li class=\"nav-item\">
+                        <a class=\"nav-link\" href=\"/logout\">Déconnexion</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+    </header>
     <body>
         ";
-        // line 12
+        // line 38
         $this->displayBlock('body', $context, $blocks);
-        // line 13
+        // line 39
         echo "        ";
         $this->displayBlock('javascripts', $context, $blocks);
-        // line 14
+        // line 40
         echo "    </body>
 </html>
 ";
@@ -113,7 +139,7 @@ class __TwigTemplate_aaf7a164deffea816be7ead945a795e51c557aa319328d00efda08a89a2
 
     }
 
-    // line 12
+    // line 38
     public function block_body($context, array $blocks = [])
     {
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
@@ -130,7 +156,7 @@ class __TwigTemplate_aaf7a164deffea816be7ead945a795e51c557aa319328d00efda08a89a2
 
     }
 
-    // line 13
+    // line 39
     public function block_javascripts($context, array $blocks = [])
     {
         $__internal_085b0142806202599c7fe3b329164a92397d8978207a37e79d70b8c52599e33e = $this->extensions["Symfony\\Bundle\\WebProfilerBundle\\Twig\\WebProfilerExtension"];
@@ -154,7 +180,7 @@ class __TwigTemplate_aaf7a164deffea816be7ead945a795e51c557aa319328d00efda08a89a2
 
     public function getDebugInfo()
     {
-        return array (  134 => 13,  117 => 12,  106 => 8,  97 => 7,  79 => 6,  67 => 14,  64 => 13,  62 => 12,  58 => 10,  56 => 7,  52 => 6,  45 => 1,);
+        return array (  160 => 39,  143 => 38,  132 => 8,  123 => 7,  105 => 6,  93 => 40,  90 => 39,  88 => 38,  58 => 10,  56 => 7,  52 => 6,  45 => 1,);
     }
 
     public function getSourceContext()
@@ -169,6 +195,32 @@ class __TwigTemplate_aaf7a164deffea816be7ead945a795e51c557aa319328d00efda08a89a2
             <link rel=\"stylesheet\" type=\"text/css\" href=\"https://bootswatch.com/4/pulse/bootstrap.min.css\">
         {% endblock %}
     </head>
+    <header>
+        <nav class=\"navbar navbar-expand-lg navbar-dark bg-primary\">
+            <a class=\"navbar-brand\" href=\"#\">Navbar</a>
+            <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarColor01\" aria-controls=\"navbarColor01\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">
+                <span class=\"navbar-toggler-icon\"></span>
+            </button>
+
+            <div class=\"collapse navbar-collapse\" id=\"navbarColor01\">
+                <ul class=\"navbar-nav mr-auto\">
+                    <li class=\"nav-item active\">
+                        <a class=\"nav-link\" href=\"#\">Blog</a>
+                    </li>
+                    <li class=\"nav-item\">
+                        <a class=\"nav-link\" href=\"/login\">Connexion</a>
+                    </li>
+                    <li class=\"nav-item\">
+                        <a class=\"nav-link\" href=\"/account\">Mon compte</a>
+                    </li>
+                    <li class=\"nav-item\">
+                        <a class=\"nav-link\" href=\"/logout\">Déconnexion</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+    </header>
     <body>
         {% block body %}{% endblock %}
         {% block javascripts %}{% endblock %}
